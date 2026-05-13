033be20711e06f11dd333fb0d96aeaaccc28b9a3
menoskar42-boop <menoskar42@gmail.com>
2026-05-13 15:34:44 +0300

    fix: remove سبب التعذر column from grouped summary table

--- client/src/components/BoxRejectionReport.tsx
+++ client/src/components/BoxRejectionReport.tsx
@@ -148,7 +148,6 @@ export function BoxRejectionReport({ orders }: BoxRejectionReportProps) {
                 <TableHead className="text-right font-bold">الكابينة</TableHead>
                 <TableHead className="text-right font-bold">رقم البوكس</TableHead>
                 <TableHead className="text-right font-bold">عدد المتعذرات</TableHead>
-                <TableHead className="text-right font-bold">سبب التعذر</TableHead>
                 <TableHead className="text-right font-bold">التفاصيل</TableHead>
               </TableRow>
             </TableHeader>
@@ -176,13 +175,6 @@ export function BoxRejectionReport({ orders }: BoxRejectionReportProps) {
                       >
                         {val.count} متعذر
                       </Badge>
-                    </TableCell>
-                    <TableCell>
-                      <div className="flex flex-col gap-1">
-                        {Array.from(val.reasons).map((r) => (
-                          <span key={r} className="text-xs text-red-700">{r}</span>
-                        ))}
-                      </div>
                     </TableCell>
                     <TableCell>
                       <Button
